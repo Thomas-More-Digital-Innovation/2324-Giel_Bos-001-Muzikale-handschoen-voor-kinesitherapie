6ad6e2f94d7a771a86b86a5f10afa3b39ea694d0
MattBoeren <r0932402@student.thomasmore.be>
2023-11-20 17:08:26 +0100

    gyro code deleted

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,7 @@
 #include <Adafruit_NeoPixel.h>       // neopixel library for ledring
 #include <SD.h>                      // sd card library
 #include <SPI.h>                     // spi library for sd card
-#include <Adafruit_MPU6050.h>        // mpu6050 library
-#include <Adafruit_Sensor.h>         // sensor library for mpu6050
-#include <Wire.h>                    // wire library for mpu6050
-#include <Adafruit_BusIO_Register.h> // busio library for mpu6050
+
 #include <vector>                    // library for dynamic array (vector)
 #include <string>
 using namespace std;
@@ -18,11 +15,6 @@ using namespace std;
 #define NOTE_G4 392
 #define NOTE_C5 523
 
-// -- pins for sensor modules -- //
-#define SCL_1 22 // pin for mpu6050 clock - I2C 1
-#define SDA_1 23 // pin for mpu6050 sda - I2C 1
-#define SCL_2 32 // pin for mpu6050 clock - I2C 2
-#define SDA_2 33 // pin for mpu6050 sda- I2C 2
 #define FSR 36   // pin for force sensor
 
 // -- pins for feedback modules -- //
@@ -33,26 +25,7 @@ using namespace std;
 // -- other defines -- //
 Adafruit_NeoPixel ring = Adafruit_NeoPixel(12, PIN_LED, NEO_GRB + NEO_KHZ800); // define ledring
 
-Adafruit_MPU6050 mpu_hand;    // mpu hand
-Adafruit_MPU6050 mpu_thumb;   // mpu thumb
-Adafruit_MPU6050 mpu_fingers; // mpu fingers
 
-// converts sensor readings to 1, -1 or 0
-int conv(int input)
-{
-  if (input < -7)
-  {
-    return -1;
-  }
-  else if (input > 7)
-  {
-    return 1;
-  }
-  else
-  {
-    return 0;
-  }
-}
 
 // - FEEDBACK MODULES - //
 // -- ledring -- //
@@ -268,261 +241,8 @@ bool isNumber(String s){
 
 void setup()
 {
-  Serial.begin(115200);      // open serial monitor
-  pinMode(14, OUTPUT);       // pin 14 is output pin
-  digitalWrite(14, HIGH);    // pin 14 output high (used for changing I2C address gyro hand)
-  Wire.begin(SDA_1, SCL_1);  // I2C bus 1
-  Wire1.begin(SDA_2, SCL_2); // I2C bus 2
-
-  while (!Serial)
-    delay(10); // pause until serial console opens
-
-  Serial.println("Adafruit MPU6050 test!");
-
-  // initialize gyro hand
-  if (!mpu_hand.begin(0x69, &Wire))
-  {
-    Serial.println("Failed to find MPU6050 hand gyro");
-    while (1)
-    {
-      delay(1000);
-    }
-  }
-  Serial.println("MPU6050 hand -- Found!");
-
-  // initialize gyro thumb
-  if (!mpu_thumb.begin(0x68, &Wire))
-  {
-    Serial.println("Failed to find MPU6050 thumb gyro");
-    while (1)
-    {
-      delay(1000);
-    }
-  }
-  Serial.println("MPU6050 thumb -- Found!");
-
-  // initialize gyro fingers
-  if (!mpu_fingers.begin(0x68, &Wire1))
-  {
-    Serial.println("Failed to find MPU6050 fingers gyro");
-    while (1)
-    {
-      delay(1000);
-    }
-  }
-  Serial.println("MPU6050 fingers -- Found!");
-
-  // Setting accelerometer ranges - hand
-  mpu_hand.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu_hand.getAccelerometerRange())
-  {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
-
-  // Setting accelerometer ranges - thumb
-  mpu_thumb.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu_thumb.getAccelerometerRange())
-  {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
-
-  // Setting accelerometer ranges - fingers
-  mpu_fingers.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu_fingers.getAccelerometerRange())
-  {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
-
-  // Setting gyro ranges - hand
-  mpu_hand.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu_hand.getGyroRange())
-  {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
-  // Setting gyro ranges - thumb
-  mpu_thumb.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu_thumb.getGyroRange())
-  {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
-  // Setting gyro ranges - fingers
-  mpu_fingers.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu_fingers.getGyroRange())
-  {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
-  // filtering bandwidth - hand
-  mpu_hand.setFilterBandwidth(MPU6050_BAND_5_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu_hand.getFilterBandwidth())
-  {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
-
-  // filtering bandwidth - thumb
-  mpu_thumb.setFilterBandwidth(MPU6050_BAND_5_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu_thumb.getFilterBandwidth())
-  {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
-
-  // filtering bandwidth - fingers
-  mpu_fingers.setFilterBandwidth(MPU6050_BAND_5_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu_fingers.getFilterBandwidth())
-  {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
 
   sensorsucces();
-
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
- 
-  Serial.print("Initializing SD card...");
- 
-  if (!SD.begin(4)) {
-    Serial.println("initialization failed!");
-    while (1);
-  }
-  Serial.println("initialization done.");
 
 
   sdsucces();
@@ -538,112 +258,11 @@ array<array<int, 3>, 3> currentex; // variable for storing exercise to be execut
 
 void loop()
 {
-  Serial.print("Number of exercises: ");
-  Serial.println(exercises.size());
-  for (int i = 0; i < exercises.size(); i++) // for every exercise
-  {
-    Serial.print("Current exercise: ");
-    Serial.println(exercises[i]);
-    currentex = selectex(exercises[i]);
-    toggle = true;
-    while (toggle)
-    {
-      // provide sensor event
-      sensors_event_t a_hand, g_hand, temp_hand;
-      sensors_event_t a_thumb, g_thumb, temp_thumb;
-      sensors_event_t a_fingers, g_fingers, temp_fingers;
-
-      // get acceleration, angular velocity and temperature from sensors (only acceleration is used)
-      mpu_hand.getEvent(&a_hand, &g_hand, &temp_hand);
-      mpu_thumb.getEvent(&a_thumb, &g_thumb, &temp_thumb);
-      mpu_fingers.getEvent(&a_fingers, &g_fingers, &temp_fingers);
-
-      // get x,y,z acceleration from hand sensor, convert & store in array
-      int x_hand = conv((int)a_hand.acceleration.x);
-      int y_hand = conv((int)a_hand.acceleration.y);
-      int z_hand = conv((int)a_hand.acceleration.z);
-      std::array<int, 3> co_hand{x_hand, y_hand, z_hand};
-
-      // get x,y,z acceleration from thumb sensor, convert & store in array
-      int x_thumb = conv((int)a_thumb.acceleration.x);
-      int y_thumb = conv((int)a_thumb.acceleration.y);
-      int z_thumb = conv((int)a_thumb.acceleration.z);
-      std::array<int, 3> co_thumb{x_thumb, y_thumb, z_thumb};
-
-      // get x,y,z acceleration from fingers sensor, convert & store in array
-      int x_fingers = conv((int)a_fingers.acceleration.x);
-      int y_fingers = conv((int)a_fingers.acceleration.y);
-      int z_fingers = conv((int)a_fingers.acceleration.z);
-      std::array<int, 3> co_fingers{x_fingers, y_fingers, z_fingers};
 
       if (exercises[i] == 0) // force sensor exercise
       {
         uint16_t fsrReading = analogRead(FSR); // analog reading from FSR
         if (fsrReading > 2500)
-        {
-          exsucces();
-          delay(500);
-          ledsoff();
-          Serial.println("SUCCES, ONTO THE NEXT EXERCISE!");
-          toggle = false;
-        }
-        else
-        {
-          Serial.println("NOT QUIET RIGHT, PLEASE TRY AGAIN!");
-        }
-        delay(1000);
-      }
-      else if (exercises[i] > 0 && exercises[i] < 7) // single positions
-      {
-        if (co_hand == currentex[0])
-        {
-          exsucces();
-          delay(500);
-          ledsoff();
-          Serial.println("SUCCES, ONTO THE NEXT EXERCISE!");
-          toggle = false;
-        }
-        else
-        {
-          Serial.println("NOT QUIET RIGHT, PLEASE TRY AGAIN!");
-        }
-        delay(1000);
-      }
-      else if (exercises[i] > 6 && exercises[i] < 9) // double positions - thumb exercises
-      {
-        if (co_hand == currentex[0] && co_thumb == currentex[1])
-        {
-          exsucces();
-          delay(500);
-          ledsoff();
-          Serial.println("SUCCES, ONTO THE NEXT EXERCISE!");
-          toggle = false;
-        }
-        else
-        {
-          Serial.println("NOT QUIET RIGHT, PLEASE TRY AGAIN!");
-        }
-        delay(1000);
-      }
-      else if (exercises[i] > 8 && exercises[i] < 13) // double positions - fingers exercises
-      {
-        if (co_hand == currentex[0] && co_fingers == currentex[2])
-        {
-          exsucces();
-          delay(500);
-          ledsoff();
-          Serial.println("SUCCES, ONTO THE NEXT EXERCISE!");
-          toggle = false;
-        }
-        else
-        {
-          Serial.println("NOT QUIET RIGHT, PLEASE TRY AGAIN!");
-        }
-        delay(1000);
-      }
-      else if (exercises[i] > 12 && exercises[i] < 15) // triple positions
-      {
-        if (co_hand == currentex[0] && co_thumb == currentex[1] && co_fingers == currentex[2])
         {
           exsucces();
           delay(500);
