b7ece566e99cadd6fb12f745093fe41da7534e5b
MattBoeren <r0932402@student.thomasmore.be>
2024-03-04 13:35:22 +0100

    with spring initilizer

---
+++ AppMuzikaleHanschoen/src/test/java/com/AppMuzikaleHanschoen/AppMuzikaleHanschoen/AppMuzikaleHanschoenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AppMuzikaleHanschoen.AppMuzikaleHanschoen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMuzikaleHanschoenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
